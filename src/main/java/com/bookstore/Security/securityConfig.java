b7e9d4d2170c0d176ba4b8f9102f42fd42ed3bec
Wassim Masmoudi <106668591+WasMas@users.noreply.github.com>
2023-02-18 16:23:50 +0100

    Add JWT Authorization Filter

--- src/main/java/com/bookstore/Security/securityConfig.java
+++ src/main/java/com/bookstore/Security/securityConfig.java
@@ -11,8 +11,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.bookstore.Security.jwt.jwtAuthorizationFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -33,18 +36,26 @@ public class securityConfig {
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
     protected void configure(HttpSecurity http) throws Exception {
         http.cors();
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeHttpRequests().requestMatchers("/api/authentication/**")
                 .permitAll().anyRequest().authenticated();
+
+        http.addFilterAt(jwtAuthorizationFilter(),UsernamePasswordAuthenticationFilter.class);
+
     }
 
-    @Bean(BeanIds.AUTHENTICATION_MANAGER)
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
-            throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
+    @Bean
+    public jwtAuthorizationFilter jwtAuthorizationFilter() {
+        return new jwtAuthorizationFilter();
     }
 
     @Bean
