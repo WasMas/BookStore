f8948c9b26d44a1e195c9c70e8644d1a8c1492ae
Wassim Masmoudi <106668591+WasMas@users.noreply.github.com>
2023-02-23 14:51:34 +0100

    Fix Purchase History Items Not displaying
    All endpoints are tested and working

--- src/main/java/com/bookstore/repositories/purchaseHistoryInt.java
+++ src/main/java/com/bookstore/repositories/purchaseHistoryInt.java
@@ -10,7 +10,7 @@ import com.bookstore.models.purchaseHistory;
 import com.bookstore.repositories.projection.purchaseItemInt;
 
 public interface purchaseHistoryInt extends JpaRepository<purchaseHistory, Long> {
-        @Query(value = "SELECT b.title as title, ph.price as price, ph.purchase_date as purchaseTime " +
-                        "FROM purchaseHistory ph LEFT JOIN book b ON ph.id_purchase = b.id_book  WHERE ph.id_user = :userId", nativeQuery = true)
+        @Query(value = "SELECT b.title as title, ph.price as price, ph.purchase_time as purchaseTime " +
+                        "FROM purchase_history ph JOIN book b ON ph.id_purchase = b.id_book  WHERE ph.id_user = :userId", nativeQuery = true)
         List<purchaseItemInt> findAllPurchasesOfUser(@Param("userId") Long userId);
 }
