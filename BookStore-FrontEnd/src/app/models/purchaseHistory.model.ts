00ce909cb757af88facc8bf040b4cb5fd758cbae
Wassim Masmoudi <106668591+WasMas@users.noreply.github.com>
2023-06-25 14:51:45 +0100

    Implement Purchase History model

---
+++ BookStore-FrontEnd/src/app/models/purchaseHistory.model.ts
@@ -0,0 +1,13 @@
+export class purchaseHistory{
+  id: number|undefined;
+  userId: number|undefined;
+  bookId: number|undefined;
+  price: number|undefined;
+  purchase: Date=new Date();
+
+  constructor(id?:number,bookId?:number,price?:number){
+    this.id=id;
+    this.bookId=bookId;
+    this.price=price;
+  }
+}
