5127d2aa0c85adbb812e478b6a16aaef29698579
Wassim Masmoudi <106668591+WasMas@users.noreply.github.com>
2023-06-25 14:57:16 +0100

    Implement purchaseItem model
    Some code formatting also

--- BookStore-FrontEnd/src/app/models/book.model.ts
+++ BookStore-FrontEnd/src/app/models/book.model.ts
@@ -1,15 +1,15 @@
 export class Book {
-  id: number|undefined;
-  title: string="";
-  author: string="";
-  price:number=0;
-  description: string="";
-  createTime:Date = new Date();
+  id: number | undefined;
+  title: string = '';
+  author: string = '';
+  price: number = 0;
+  description: string = '';
+  createTime: Date = new Date();
 
   // "?" = can be underfined
-  constructor(id?:number,title: string="", price:number=0){
-    this.id=id;
-    this.title=title;
-    this.price=price;
+  constructor(id?: number, title: string = '', price: number = 0) {
+    this.id = id;
+    this.title = title;
+    this.price = price;
   }
-} 
+}
--- BookStore-FrontEnd/src/app/models/purchaseHistory.model.ts
+++ BookStore-FrontEnd/src/app/models/purchaseHistory.model.ts
@@ -1,13 +1,13 @@
-export class purchaseHistory{
-  id: number|undefined;
-  userId: number|undefined;
-  bookId: number|undefined;
-  price: number|undefined;
-  purchase: Date=new Date();
+export class purchaseHistory {
+  id: number | undefined;
+  userId: number | undefined;
+  bookId: number | undefined;
+  price: number | undefined;
+  purchase: Date = new Date();
 
-  constructor(id?:number,bookId?:number,price?:number){
-    this.id=id;
-    this.bookId=bookId;
-    this.price=price;
+  constructor(id?: number, bookId?: number, price?: number) {
+    this.id = id;
+    this.bookId = bookId;
+    this.price = price;
   }
 }
