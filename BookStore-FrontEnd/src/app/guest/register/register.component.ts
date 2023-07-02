7e73832e60235626c6006a2b21466703a2c4cd67
Wassim Masmoudi <106668591+WasMas@users.noreply.github.com>
2023-07-02 17:36:17 +0100

    Register Component ts

--- BookStore-FrontEnd/src/app/guest/register/register.component.ts
+++ BookStore-FrontEnd/src/app/guest/register/register.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { faUserCircle } from '@fortawesome/free-regular-svg-icons';
+import { User } from 'src/app/models/user.model';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  user: User = new User();
+  faUser = faUserCircle;
+  errorMessage: String = '';
 
+  constructor(
+    private authenticationService: AuthenticationService,
+    private router: Router
+  ) {}
+  ngOnInit(): void {
+    if (this.authenticationService.currentUserValue?.id) {
+      this.router.navigate(['/profile']);
+      return;
+    }
+  }
+
+  register() {
+    this.authenticationService.register(this.user).subscribe(
+      (data) => {
+        this.router.navigate(['/login']);
+      },
+      (err) => {
+        if (err?.status === 409) {
+          this.errorMessage = 'Username Already Exists';
+        } else {
+          this.errorMessage = 'Unexpected error' + this.errorMessage;
+          console.log(err);
+        }
+      }
+    );
+  }
 }
