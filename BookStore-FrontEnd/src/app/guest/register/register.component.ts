cfc0ffb93ca3f8553b7771125e4d89305ab6f8ee
Wassim Masmoudi <106668591+WasMas@users.noreply.github.com>
2023-07-08 15:42:26 +0100

    Complete Login Page

--- BookStore-FrontEnd/src/app/guest/register/register.component.ts
+++ BookStore-FrontEnd/src/app/guest/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { faUserCircle } from '@fortawesome/free-regular-svg-icons';
 import { User } from 'src/app/models/user.model';
@@ -9,7 +9,7 @@ import { AuthenticationService } from 'src/app/services/authentication.service';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   user: User = new User();
   faUser = faUserCircle;
   errorMessage: String = '';
@@ -18,6 +18,7 @@ export class RegisterComponent {
     private authenticationService: AuthenticationService,
     private router: Router
   ) {}
+
   ngOnInit(): void {
     if (this.authenticationService.currentUserValue?.id) {
       this.router.navigate(['/profile']);
