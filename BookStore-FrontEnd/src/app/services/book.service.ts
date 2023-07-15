2d89c7a9e4a64c54a57123ba8b3b814e4d53c60d
Wassim Masmoudi <106668591+WasMas@users.noreply.github.com>
2023-07-15 15:40:32 +0100

    Implement Books Service

---
+++ BookStore-FrontEnd/src/app/services/book.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
+import { RequestBaseService } from './request-base.service';
+import { AuthenticationService } from './authentication.service';
+import { HttpClient } from '@angular/common/http';
+import { Book } from '../models/book.model';
+import { Observable } from 'rxjs';
+
+const API_URL = `${environment.BASE_URL}/api/book`;
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BookService extends RequestBaseService {
+  constructor(
+    authenticationService: AuthenticationService,
+    httpclient: HttpClient
+  ) {
+    super(authenticationService, httpclient);
+  }
+
+  saveBook(book: Book): Observable<any> {
+    return this.httpClient.post(API_URL, book, { headers: this.headers });
+  }
+  deleteBook(book: Book): Observable<any> {
+    return this.httpClient.delete(API_URL + '/' + book.id_book, {
+      headers: this.headers,
+    });
+  }
+  getAllbooks(book: Book): Observable<any> {
+    return this.httpClient.get(API_URL);
+  }
+}
