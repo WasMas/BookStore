553ebb1b47b40a4979bd43c44e8b7e928cde7981
Wassim Masmoudi <106668591+WasMas@users.noreply.github.com>
2023-07-24 20:32:26 +0100

    First Stable Release of the app

--- BookStore-BackEnd/src/main/java/com/bookstore/Security/securityConfig.java
+++ BookStore-BackEnd/src/main/java/com/bookstore/Security/securityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 import com.bookstore.Security.jwt.jwtAuthorizationFilter;
 import com.bookstore.models.role;
@@ -51,15 +52,17 @@ public class securityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors();
-        http.csrf().disable();
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.authorizeHttpRequests()
-                .requestMatchers("/api/authentication/**").permitAll()
-                .requestMatchers("/api/internal/**").hasRole(role.SYSTEM_MANAGER.name())
-                .requestMatchers(HttpMethod.GET,"/api/book").permitAll()
-                .requestMatchers("/api/book/**").hasRole(role.Admin.name())
-                .anyRequest().authenticated();
+        http.cors(withDefaults());
+        http.csrf(csrf -> csrf.disable());
+        http.sessionManagement(
+                sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.authorizeHttpRequests(
+                authorizeHttpRequests -> authorizeHttpRequests
+                        .requestMatchers("/api/authentication/**").permitAll()
+                        .requestMatchers("/api/internal/**").hasRole(role.SYSTEM_MANAGER.name())
+                        .requestMatchers(HttpMethod.GET, "/api/book").permitAll()
+                        .requestMatchers("/api/book/**").hasRole(role.Admin.name())
+                        .anyRequest().authenticated());
 
         // * JWT Filter
         // Internal > jwt > authentication
